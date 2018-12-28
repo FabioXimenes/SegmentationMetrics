df8eae1d6ebc8515051d7375bd7a3a24303a0989
FabioXimenes <fabio.ximenes@lapisco.ifce.edu.br>
2018-12-28 13:48:08 -0300

    Remove some unused code

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import cv2
 import argparse
 import findpaths
 import metrics
-import csv
 
 # Construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -10,16 +9,7 @@ ap.add_argument('-s', '--segmented-images', type=str, required=True,
                 help='Path to the segmented images.')
 ap.add_argument('-g', '--ground-truth', type=str, required=True,
                 help='Path to the ground truth images')
-ap.add_argument('-r', '--results', type=str, required=False,
-                help='Path to the outputs results. If not used the '
-                     'results will be saved on the same path of the segmented images')
-'''
- Verify if it is possible to print directly on LaTeX format
 
-ap.add_argument('-o', '--output-type', type=str, required=False, default='csv',
-                help='Type of the output file. Accepts txt, xls and csv.'
-                     'If not used the results will be saved on csv format.')
-'''
 args = vars(ap.parse_args())
 
 # Pick all the paths to the segmented images
@@ -39,7 +29,8 @@ if len(segmentedPaths) != len(gtPaths):
     print('The number of images to be compared must be of the same size!')
     exit()
 
-metrics_list = ['Jac', 'Mcc', 'Dice', 'Acc', 'Sen', 'Spe', 'Ppv', 'Npv', 'Fpr', 'Fdr', 'Fnr', 'F1-Score']
+# Create a list of the metrics that will be calculated
+metrics_list = ['Jac', 'Mcc', 'Dice', 'Acc', 'Sen', 'Spe', 'F1-Score', 'Ppv', 'Npv', 'Fpr', 'Fdr', 'Fnr']
 
 # Create a dictionary that will contain all the metrics as keys and their respective values for each image
 dict_values = {}
